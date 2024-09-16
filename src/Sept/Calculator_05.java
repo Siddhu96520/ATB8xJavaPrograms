f85c1c5828f0e8189677ba07a97328f13a209c7f
Siddhu96520 <siddhu96520@gmail.com>
2024-09-17 00:35:58 +0530

    just commit the selected files

---
+++ src/Sept/Calculator_05.java
@@ -0,0 +1,19 @@
+package Sept;
+
+public class Calculator_05 {
+
+    public static void main(String[] args){
+
+        int a=30;
+        int b=10;
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+
+
+    }
+}
---
+++ src/Sept/PrintTable_06.java
@@ -0,0 +1,26 @@
+package Sept;
+
+public class PrintTable_06 {
+
+    public static void main(String[] args){
+
+
+
+        int num=10;
+
+        System.out.printf("%d x %d  = %d",num,1,num*1);
+        System.out.println();
+
+        System.out.printf("%d x %d  = %d",num,2,num*2);
+        System.out.println();
+
+        System.out.printf("%d x %d  = %d",num,3,num*3);
+        System.out.println();
+
+        System.out.printf("%d x %d  = %d",num,4,num*4);
+        System.out.println();
+
+    }
+
+
+}
